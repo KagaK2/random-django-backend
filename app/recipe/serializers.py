534185ef13421db27b5b1dc63a708e401fec92ed
KagaK2 <KhanhPhan2512@gmail.com>
2020-05-12 00:06:39 +0300

    recipe details

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -22,7 +22,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         many=True,
         queryset=Ingredient.objects.all()
     )
-    tags = serializers.PrimaryKeyRelatedField(
+    tags  = serializers.PrimaryKeyRelatedField(
         many=True,
         queryset=Tag.objects.all()
     )
@@ -31,3 +31,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes', 'price', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
